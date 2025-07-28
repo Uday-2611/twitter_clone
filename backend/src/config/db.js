ab27e4c0950b5d17a0194443b968f1caab3967a4
Uday-2611 <udayagarwal234@gmail.com>
2025-07-28 15:00:48 +0530

    created models

---
+++ backend/src/config/db.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+import { ENV } from './env.js';
+
+export const connectDB = async () => {
+    try {
+
+        await mongoose.connect(ENV.MONGO_URI);
+        console.log('Connected to Database Successfully');
+        
+    } catch (error) {
+        console.log('Error connecting to Database')
+        process.exit(1);
+    }
+}
