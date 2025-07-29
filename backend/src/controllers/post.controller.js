88301747ebf4a7cda8d9a2c7ec594c3201345f34
Uday-2611 <udayagarwal234@gmail.com>
2025-07-29 20:12:20 +0530

    bug fixes

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -1,9 +1,10 @@
 import asyncHandler from 'express-async-handler'
-import Post from '../models/post.model.js';
-import User from '../models/user.model.js';
 import { getAuth } from '@clerk/express';
 import cloudinary from '../config/cloudinary.js';
+import Post from '../models/post.model.js';
+import User from '../models/user.model.js';
 import Notification from '../models/notification.model.js';
+import Comment from '../models/comment.model.js'
 
 // Public Routes -> 
 
