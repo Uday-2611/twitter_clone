fe9e5ca936759333a6d1a81ae63a40324ccd3da1
Uday-2611 <udayagarwal234@gmail.com>
2025-07-30 01:33:33 +0530

    created comment routes and controllers

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,6 +7,7 @@ import { clerkMiddleware } from '@clerk/express';
 
 import userRoutes from './routes/user.route.js'
 import postRoutes from './routes/post.route.js'
+import commentRoutes from './routes/comment.route.js'
 
 const app = express();
 
@@ -18,6 +19,7 @@ app.get("/", (req, res) => res.send('Hello from server'));
 
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
+app.use('/api/comments', commentRoutes);
 
 //error handling middleware -> 
 app.use((err, req, res, next) => {
