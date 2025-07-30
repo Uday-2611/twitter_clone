32b0a2590f3d6f1e570383cb593a733da12af778
Uday-2611 <udayagarwal234@gmail.com>
2025-07-30 18:14:08 +0530

    auth implemented

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,7 +1,17 @@
+import { ClerkProvider } from '@clerk/clerk-expo'
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
 import { Stack } from "expo-router";
 import '../global.css';
 
+// Stack navigator - we can get the latest page at the top and the oldest page at the bottom and is used for tab navigation. 
+
 export default function RootLayout() {
-  return <Stack />
-  // Stack navigator - we can get the latest page at the top and the oldest page at the bottom and is used for tab navigation. 
+  return (
+    <ClerkProvider tokenCache={tokenCache}>
+      <Stack>
+        <Stack.Screen name='(auth)' options={{ headerShown: false }} />
+      </Stack>
+    </ClerkProvider>
+  )
+
 }
---
+++ mobile/hooks/useSocialAuth.ts
@@ -0,0 +1,33 @@
+import { useSSO } from "@clerk/clerk-expo";
+import { useState } from "react"
+import { Alert } from "react-native";
+
+export const useSocialAuth = () => {
+
+    const [isLoading, setIsLoading] = useState(false);
+    const { startSSOFlow } = useSSO();
+
+    const handleSocialAuth = async (strategy: 'oauth_google' | 'oauth_apple') => {
+        setIsLoading(true);
+        try {
+
+            const { createSessionId, setActive } = await startSSOFlow({ strategy });
+
+            if (createSessionId && setActive) {
+                await setActive({ session: createSessionId });
+            }
+
+        } catch (error) {
+            console.log('Error in social auth', error);
+            const provider = strategy === 'oauth_google' ? 'Google' : 'Apple'
+
+            Alert.alert('Error', `Failed to sign in with ${provider}. Please try again`)
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    return { isLoading, handleSocialAuth }
+}
+
+// TODO: What is strategy in auth 
