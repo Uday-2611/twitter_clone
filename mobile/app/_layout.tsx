a33a4dad8bf2c61372ebe19a65f3af2fc6e49542
Uday-2611 <udayagarwal234@gmail.com>
2025-08-06 23:52:26 +0530

    minor changes

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,6 +1,7 @@
 import { ClerkProvider } from '@clerk/clerk-expo'
 import { tokenCache } from '@clerk/clerk-expo/token-cache'
 import { Stack } from "expo-router";
+import {StatusBar} from 'expo-status-bar';
 import '../global.css';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
@@ -16,7 +17,10 @@ export default function RootLayout() {
       <QueryClientProvider client={queryClient}>
         <Stack screenOptions={{ headerShown: false }}>
           <Stack.Screen name='(auth)' />
+          <Stack.Screen name='(tabs)' />
         </Stack>
+
+        <StatusBar style='dark' />
       </QueryClientProvider>
     </ClerkProvider>
   )
