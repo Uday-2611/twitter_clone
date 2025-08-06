2caa34ed95b45d6c7bd215511fdef75ebbf820d8
Uday-2611 <udayagarwal234@gmail.com>
2025-08-06 23:50:25 +0530

    implemented notifications page

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,13 +1,23 @@
-import { ScrollView, Text, View } from 'react-native'
-import React from 'react'
+import { RefreshControl, ScrollView, Text, View } from 'react-native'
+import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import SignOutButton from '@/components/SignOutButton'
 import { useUserSync } from '@/hooks/useUserSync'
 import { Ionicons } from '@expo/vector-icons'
 import PostComposer from '@/components/PostComposer'
 import PostsList from '@/components/PostList'
+import { usePosts } from '@/hooks/usePosts'
 
 const HomeScreen = () => {
+
+  const [isRefetching, setIsRefetching] = useState(false);
+  const { refetch: refetchPosts } = usePosts()
+
+  const handlePullToRefresh = async () => {
+    setIsRefetching(true);
+    await refetchPosts();
+    setIsRefetching(false);
+  }
 
   useUserSync();
 
@@ -25,6 +35,14 @@ const HomeScreen = () => {
         showsVerticalScrollIndicator={false}
         className='flex-1'
         contentContainerStyle={{ paddingBottom: 80 }}
+
+        refreshControl={
+          <RefreshControl
+            refreshing={isRefetching}
+            onRefresh={handlePullToRefresh}
+            tintColor={'#1DA1F2'}
+          />
+        }
       >
         <PostComposer />
 
